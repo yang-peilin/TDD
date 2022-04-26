0312c70cf24e4698723e38132cbfb010b3af2070
yang-peilin <peilin.yang.bupt@outlook.com>
2022-04-26 08:31:13 +0800

    Functional test now checks we can input a to-do item

--- functionTest.py
+++ functionTest.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,22 +12,40 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-    # edith has heard about a cool new online to-do app. she goes to check out its homepage
+        # edith has heard about a cool new online to-do app. she goes to check out its homepage
         self.browser.get('http://localhost:8000')
-    # she notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title), "Browser title was: " + self.browser.title
+
+        # she notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # she is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # she types "buy peacock feaathers" into a text book (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # when sha hits enter, the page updates, and now the page lists
+        # 1. buy peacock feathers as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Bug peacock feathers' for row in rows)
+        )
+
+        # there is still a text box inviting her to add another item, she enters "Use peacock feathers to make a fly" (edith is very methodical)
         self.fail('Finish the test!')
 
 
-# she is invited to enter a to-do item straight away
 
-# she types "buy peacock feaathers" into a text book (Edith's hobby is tying fly-fishing lures)
-
-
-# when sha hits enter, the page updates, and now the page lists
-# 1. buy peacock feathers as an item in a to-do list
-
-# there is still a text box inviting her to add another item, she enters "Use peacock feathers to make a fly" (edith is very methodical)
 
 # the page updates again, and now shows both items on her list
 
