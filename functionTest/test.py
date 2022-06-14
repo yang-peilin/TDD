c2520c0a46a5551d1948beeba13649a14b1deb56
yangpeilin <peilin.yang.bupt@outlook.com>
2022-06-14 13:16:32 +0800

    first steps of FT for layout + styling

--- functionTest/test.py
+++ functionTest/test.py
@@ -15,6 +15,23 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -109,6 +126,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfies, they both go back to sleep
+
 
 
 # edith wonders whether the site will remember her list. then she sees that the site has generated a unique url her -- there is some
